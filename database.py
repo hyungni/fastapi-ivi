05b21a9bde81742de046936273840ea3ee5d6806
류형근 <hyungni@gmail.com>
2024-11-18 14:45:15 +0900

    postgresql 적용

--- database.py
+++ database.py
@@ -7,9 +7,17 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
 # config = Config('.env')
 # SQLALCHEMY_DATABASE_URL = config('SQLALCHEMY_DATABASE_URL')
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
