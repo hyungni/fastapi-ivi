f89bd0ee95d8654d80c70bfc1b886c22be614c5f
류형근 <hyungni@gmail.com>
2024-11-19 13:45:51 +0900

    config사용

--- database.py
+++ database.py
@@ -3,20 +3,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from starlette.config import Config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
-# config = Config('.env.production')
-# SQLALCHEMY_DATABASE_URL = config('SQLALCHEMY_DATABASE_URL')
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
+config = Config('./frontend/.env.production')
+SQLALCHEMY_DATABASE_URL = config("SQLALCHEMY_DATABASE_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
-# if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
-#     engine = create_engine(
-#         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-#     )
-# else:
-#     engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
